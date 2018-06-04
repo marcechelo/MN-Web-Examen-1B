4cc7d772023de071d77b86b5a64a4f27c7768c7f
Marcelo Nieto <marcechelo25@hotmail.com>
2018-06-03 22:39:51 -0500

    1

--- src/pelicula.controller.ts
+++ src/pelicula.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, NotFoundException, Post, Put, Req, Res } from '@nestjs/common';
-import { ActorService } from './actor.service';
 import { ActorBadRequestPipe } from './pipes/actor-bad-request.pipe';
 import { PELICULA_SCHEMA } from './pelicula/pelicula.schema';
 import { PeliculaService } from './pelicula.service';
